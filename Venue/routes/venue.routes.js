5dbd39f4ef0716e28de18b73ce59158d538559ac
saputhebeast <sapumalwijekoon4@gmail.com>
2023-04-18 01:17:47 +0530

    Venue part completed

--- Venue/routes/venue.routes.js
+++ Venue/routes/venue.routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { createVenue } = require('../controllers/venue.controller');
 const { protect, venueManagerProtect } = require('../../User/middleware/authMiddleware');
+const { createVenue, getAllVenues, getVenueById, getVenuesByManagerId, updateVenueById, deleteVenueById } = require('../controllers/venue.controller');
 
 const venueRouter = express.Router();
 
 venueRouter.post('/', protect, venueManagerProtect, createVenue)
-// venueRouter.get('/', protect, getAllVenues)
-// venueRouter.get('/:id', getVenueById)
-//venueRouter.get('/manager/:id', getVenuesByManagerId)
-// venueRouter.put('/:id', updateVenueById)
-// venueRouter.delete('/:id', deleteVenueById)
+venueRouter.get('/', getAllVenues)
+venueRouter.get('/:id', getVenueById)
+venueRouter.get('/manager/:id', getVenuesByManagerId)
+venueRouter.put('/:id', updateVenueById)
+venueRouter.delete('/:id', deleteVenueById)
 
 module.exports = venueRouter;
