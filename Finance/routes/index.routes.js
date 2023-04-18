3b21aa94be838e56b99432946a15a37dc7c7d2c1
Thareendra2000 <thareendra2000@gmail.com>
2023-04-19 05:12:34 +0530

    Completed Billing

--- Finance/routes/index.routes.js
+++ Finance/routes/index.routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 
 const paymentRouter = require('./payment.routes');
+const billingRouter = require('./billing.routes');
 
 router.use('/payments', paymentRouter);
+router.use('/billings', billingRouter);
 
 module.exports = router;
