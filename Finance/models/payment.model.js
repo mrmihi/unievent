a11213f735076dcfd124feb9ee773f20412cffaa
Thareendra2000 <thareendra2000@gmail.com>
2023-04-18 16:15:32 +0530

    Added payment.model.js

---
+++ Finance/models/payment.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const STATUS = require('../constants/payment.constant');
+
+const PaymentSchema = new Schema(
+  {
+    start_time: { type: Date, required: true },
+    end_time: { type: Date, required: true },
+    duration: { type: Number, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: STATUS,
+    },
+    venue: { type: Schema.Types.ObjectId, ref: 'Venue', required: true },
+    price: { type: Number, required: true },
+    organizer: {
+      type: Schema.Types.ObjectId,
+      ref: 'Organizer',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Payment = mongoose.model('Payment', PaymentSchema);
+
+module.exports = Payment;
