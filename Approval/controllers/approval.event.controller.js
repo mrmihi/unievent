e1b331ec9d78de29a6f85f402502adf88c8e4f5a
Shabina <shabinafarveen@gmail.com>
2023-04-18 22:20:11 +0530

    Create Event Approval, Get All Event Approvals, Get Event Approval By ID (Own ID) Controllers done

--- Approval/controllers/approval.event.controller.js
+++ Approval/controllers/approval.event.controller.js
@@ -3,11 +3,26 @@ const { HTTP_STATUS } = require('../utils/http_status');
 const { makeResponse } = require('../utils/response');
 const { Event_Approval } = require('../models/approval.model');
 
-const getAllEventApprovals = async (req, res) => {}
+const getAllEventApprovals = async (req, res) => {
+    try {
+        const eventApprovals = await Event_Approval.find({}) 
+        return makeResponse({
+            res,
+            message: 'Event Approvals fetched successfully',
+            data: eventApprovals,
+        });
+    } catch (error) {
+        return makeResponse({
+            res,
+            success : false,
+            message: error.message,
+            data: req.body,
+        });
+    }
+}
 
 const createEventApproval = async (req, res) => {
     try {
-        console.log(req.body);
         const eventApproval = await EventApprovalService.createEventApproval(req.body);
         return makeResponse({
             res,
@@ -15,7 +30,6 @@ const createEventApproval = async (req, res) => {
             data: eventApproval,
         });
     } catch (error) {
-        console.log(error.message);
         return makeResponse({
             res,
             success : false,
@@ -25,7 +39,33 @@ const createEventApproval = async (req, res) => {
     }
 }
 const getEventApproval = async (req, res) => {
-    
+    try {
+        const {_id: eventApprovalID } = req.params;
+        const eventApproval = await Event_Approval.findById({_id: eventApprovalID});
+
+        if(!eventApproval){
+            return makeResponse({
+                res,
+                success: false,
+                message: `no recipient with id: ${recipientID}`,
+                data : req.body,
+            }) 
+        }
+
+        return makeResponse({
+            res,
+            message: 'Event Approval fetched successfully',
+            data: eventApproval,
+        });
+
+    } catch (error) {
+        return makeResponse({
+            res,
+            success : false,
+            message: error.message,
+            data: req.body,
+        });
+    }
 }
 const updateEventApproval = async (req, res) => {}
 const deleteEventApproval = async (req, res) => {}
