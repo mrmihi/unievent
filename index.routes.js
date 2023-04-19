fcd6e69812d32377ba1817c43fd597be68251f97
Radser2001 <supuni.randeniya@gmail.com>
2023-04-19 09:43:54 +0530

    Update index.route.js

--- index.routes.js
+++ index.routes.js
@@ -5,11 +5,13 @@ const routes = express.Router();
 const eventsRouter = require('./Event/routes/index.routes');
 const venueRouter = require('./Venue/routes/index.routes');
 const userRouter = require('./User/routes/index.routes');
+const approvalRouter = require('./Approval/routes/index.routes');
 const partnerRouter = require('./Partners/routes/index.routes');
 
 routes.use('/', eventsRouter);
 routes.use('/', venueRouter);
 routes.use('/', userRouter);
+routes.use('/', approvalRouter);
 routes.use('/partners', partnerRouter);
 
 module.exports = routes;
