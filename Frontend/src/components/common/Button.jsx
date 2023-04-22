3e1a40961cab2b742df02889838cbf4d0d287814
MiHi <mrmihiraj@gmail.com>
2023-04-22 12:57:00 +0530

    Deleted the frontend

--- Frontend/src/components/common/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Button = ({ children, ...props }) => {
-    return (
-        <button {...props} className="">{children}</button>
-    )
-}
-
-export default Button
