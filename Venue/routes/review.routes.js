404a98540252d2938d1ccc15e9c5d95e8eb62a4f
saputhebeast <sapumalwijekoon4@gmail.com>
2023-04-26 11:48:16 +0530

    Backend integrated

--- Venue/routes/review.routes.js
+++ Venue/routes/review.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   protect,
   authOrg,
+  organizationProtect
 } = require('../../User/middleware/orgAuth.middleware');
 const {
   createReview,
@@ -10,17 +11,19 @@ const {
   updateReviewById,
   deleteReviewById,
   getReviewsByVenueId,
-  getReviewsByOrganizerId
+  getReviewsByOrganizerId,
+  getReviewsByVenueManager,
 } = require('../controllers/review.controller');
 
 const reviewRouter = express.Router();
 
-reviewRouter.post('/', protect, authOrg, createReview);
+reviewRouter.post('/', protect, createReview);
 reviewRouter.get('/', getAllReviews);
 reviewRouter.get('/:id', getReviewById);
 reviewRouter.put('/:id', updateReviewById);
 reviewRouter.delete('/:id', deleteReviewById);
 reviewRouter.get('/venue/:id', getReviewsByVenueId)
-reviewRouter.get('/organizer/:id', protect, authOrg, getReviewsByOrganizerId)
+reviewRouter.get('/organizer/:id', protect, organizationProtect, getReviewsByOrganizerId)
+reviewRouter.get('/venue-manager/:id', getReviewsByVenueManager)
 
 module.exports = reviewRouter;
