7ea257ad4e059c352fb98d01fa4ed237ed7eed48
MiHi <mrmihiraj@gmail.com>
2023-04-26 16:52:33 +0530

    Modified the event page

--- frontend/src/event/SingleEvent.jsx
+++ frontend/src/event/SingleEvent.jsx
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router-dom';
 import eventService from './event.service';
 import { useEffect, useState } from 'react';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { WidthNormal } from '@mui/icons-material';
 
 function SingleEvent() {
   const { id } = useParams();
@@ -10,7 +11,6 @@ function SingleEvent() {
 
   useEffect(() => {
     eventService.getOne(id).then((res) => {
-      console.log('I the useeffect ran');
       setEvent(res.data);
     });
   }, []);
@@ -19,20 +19,34 @@ function SingleEvent() {
 
   if (!event) return <h1>Loading...</h1>;
   return (
-    <div>
-      <h1>Single Event</h1>
-      <h2>{id}</h2>
-      <h2>{event.name}</h2>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          navigate(`/events/${id}/register`);
-        }}
-      >
-        Register
-      </Button>
-    </div>
+    <>
+      <div className="flex flex-col justify-center  items-center ">
+        <img src={event.headerImage} alt={event.name} className="mx-auto" />
+
+        <div className="  mt-8">
+          <div>
+            <Typography variant="h2">{event.name}</Typography>
+            <Typography variant="h3">{event.description}</Typography>
+            <Typography variant="h4">{event.category}</Typography>
+            <Typography variant="h4">Capacity : {event.capacity}</Typography>
+            <Typography variant="h4">Start Time : {event.startTime}</Typography>
+            <Typography variant="h4">End Time : {event.endTime}</Typography>
+          </div>
+        </div>
+        <div className="mt-8 text-center ml-12 mr-12 mb-6">
+          <Button
+            sx={{ width: 300 }}
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              navigate(`/events/${id}/register`);
+            }}
+          >
+            Register
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
 
