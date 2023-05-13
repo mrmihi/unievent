804dc1a0d4ccc217ec9e07a4b88951a95d259d84
sapumalwijekoon4@gmail.com <sapumalwijekoon4@gmail.com>
2023-05-14 00:02:02 +0530

    Update

--- Venue/routes/index.routes.js
+++ Venue/routes/index.routes.js
@@ -4,10 +4,12 @@ const venueRouter = require('./venue.routes');
 const reviewRouter = require('./review.routes');
 const bookingRouter = require('./booking.routes');
 const dashboardRouter = require('./dashboard.routes');
+const subscribeRouter = require('./subscribe.routes');
 
 router.use('/venues', venueRouter);
 router.use('/reviews', reviewRouter);
 router.use('/bookings', bookingRouter);
 router.use('/dashboard', dashboardRouter);
+router.use('/subscribe', subscribeRouter);
 
 module.exports = router;
