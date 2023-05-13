dd4bad2af32fbd7feafaef4740d962b22d4a07fb
Thareendra2000 <thareendra2000@gmail.com>
2023-05-13 18:28:30 +0530

    Edited App.js and package json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,16 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
-import Layout from "./Finance/scenes/layout";
-import Dashboard from "Finance/scenes/dashboard";
-import Overview from "Finance/scenes/finance/overview";
-import LoginPage from "Finance/scenes/login";
-import Refunds from "Finance/scenes/finance/refunds";
-import Table from "./Finance/scenes/finance/table";
-import Bills from "./Finance/scenes/finance/bills";
-import Payments from "./Finance/scenes/finance/payments";
-import Report from "./Finance/scenes/finance/report";
-import PayPal from "Finance/scenes/finance/paymentform";
+import FLayout from "./Finance/scenes/layout";
+import FDashboard from "Finance/scenes/dashboard";
+import FOverview from "Finance/scenes/finance/overview";
+import FLoginPage from "Finance/scenes/login";
+import FRefunds from "Finance/scenes/finance/refunds";
+import FTable from "./Finance/scenes/finance/table";
+import FBills from "./Finance/scenes/finance/bills";
+import FPayments from "./Finance/scenes/finance/payments";
+import FReport from "./Finance/scenes/finance/report";
+import FPayPal from "Finance/scenes/finance/paymentform";
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
@@ -26,18 +26,18 @@ function App() {
 
                     {/* Finance Routes */}
                     <Routes>
-                        <Route path="/finance/paypal" element={<PayPal />} />
+                        <Route path="/finance/paypal" element={<FPayPal />} />
                         <Route path="*" element={<h1>Page not found!</h1>} />
-                        <Route path="/admin/finance" element={<LoginPage />} />
-                        <Route element={<Layout />}>
+                        <Route path="/admin/finance" element={<FLoginPage />} />
+                        <Route element={<FLayout />}>
                             <Route path="/admin/finance/dashboard/*" element={<Navigate to="/admin/finance/dashboard" replace />} />
-                            <Route path="/admin/finance/dashboard" element={<Dashboard />} />
-                            <Route path="/admin/finance/overview" element={<Overview />} />
-                            <Route path="/admin/finance/refunds" element={<Refunds />} />
-                            <Route path="/admin/finance/table" element={<Table />} />
-                            <Route path="/admin/finance/bills" element={<Bills />} />
-                            <Route path="/admin/finance/payments" element={<Payments />} />
-                            <Route path="/admin/finance/report" element={<Report />} />
+                            <Route path="/admin/finance/dashboard" element={<FDashboard />} />
+                            <Route path="/admin/finance/overview" element={<FOverview />} />
+                            <Route path="/admin/finance/refunds" element={<FRefunds />} />
+                            <Route path="/admin/finance/table" element={<FTable />} />
+                            <Route path="/admin/finance/bills" element={<FBills />} />
+                            <Route path="/admin/finance/payments" element={<FPayments />} />
+                            <Route path="/admin/finance/report" element={<FReport />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>
