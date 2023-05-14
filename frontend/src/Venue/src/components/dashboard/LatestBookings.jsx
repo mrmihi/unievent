ef3aeebf88d1706dbd31f3e013a4bc7ee53b01c1
sapumalwijekoon4@gmail.com <sapumalwijekoon4@gmail.com>
2023-05-14 11:47:59 +0530

    Dashboard updated

--- frontend/src/Venue/src/components/dashboard/LatestBookings.jsx
+++ frontend/src/Venue/src/components/dashboard/LatestBookings.jsx
@@ -1,45 +1,24 @@
 import React, { useState } from "react";
 import { Box, Typography, Table, TableBody, TableRow, TableCell } from "@mui/material";
+import { useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const LatestBookings = () => {
-    const [latestBookings, setLatestBookings] = useState([
-        {
-            venueName: "Venue A",
-            eventName: "Event 1",
-            price: 200,
-            hall: "Hall 1",
-            time: "2023-04-21",
-        },
-        {
-            venueName: "Venue B",
-            eventName: "Event 2",
-            price: 350,
-            hall: "Hall 2",
-            time: "2023-03-29",
-        },
-        {
-            venueName: "Venue C",
-            eventName: "Event 3",
-            price: 500,
-            hall: "Hall 3",
-            time: "2023-03-27",
-        },
-        {
-            venueName: "Venue D",
-            eventName: "Event 4",
-            price: 150,
-            hall: "Hall 4",
-            time: "2023/03/11",
-        },
-        {
-            venueName: "Venue D",
-            eventName: "Event 4",
-            price: 150,
-            hall: "Hall 4",
-            time: "2023/03/11",
-        },
-    ]);
-    
+    const [latestBookings, setLatestBookings] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/api/dashboard/bookings", {
+            headers: {
+                Authorization: `Bearer ${Cookies.get("accessToken")}`,
+            },
+        }).then((response) => {
+            setLatestBookings(response.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
     return (
         <Box ml={2}>
             <Typography variant="h5" gutterBottom mb={2}>
@@ -51,15 +30,15 @@ const LatestBookings = () => {
                         <TableRow key={index}>
                             <TableCell>
                                 <Typography variant="subtitle1">
-                                    {booking.venueName} - {booking.eventName}
+                                    Event - {booking.event.name}
                                 </Typography>
                                 <Typography variant="body1" color="textSecondary">
-                                    Price: ${booking.price} | Hall: {booking.hall}
+                                    Price: ${booking.price} | Hall: {booking.venue.name}
                                 </Typography>
                             </TableCell>
                             <TableCell align="right">
                                 <Typography variant="body2" color="textSecondary">
-                                    {booking.time}
+                                    {booking.created_at}
                                 </Typography>
                             </TableCell>
                         </TableRow>
