14546434e58b1875318893d42733ecafbdf8959c
maleeshagimmana <it21176456@my.sliit.lk>
2023-05-15 19:56:23 +0530

    Feat

--- Budget/models/budget.models.js
+++ Budget/models/budget.models.js
@@ -1,36 +1,61 @@
 const mongoose = require('mongoose');
 
 const budgetSchema = new mongoose.Schema({
-    name: {
+    eventName: {
         type: String,
-        required: true
-    },
-    amount: {
-        type: Number,
-        required: true
-    },
-    categories:[
-        {
-            name:String,
-            expenses:[
-                {
-                    name:String,
-                    amount:Number,
-                }
-            ]
-        }
-    ],
-
-    createdBy:{
-        type: mongoose.Schema.Types.ObjectId,
-
-        ref: 'User'
-    },
-    createdAt:{
+        // required: true
+      },
+      organizationName: {
+        type: String,
+        // required: true
+      },
+      createdDate: {
         type: Date,
         default: Date.now
-    }
+      },
+    //   creatorEmail: {
+    //     type: String,
+    //     required: true
+    //   },
+      income: [
+        {
+          description: String,
+          amount: Number
+        }
+      ],
+      expenses: [
+        {
+          description: String,
+          amount: Number
+        }
+      ],
+      totalIncome: {
+        type: Number,
+        default: function() {
+          return this.income.reduce((acc, inc) => acc + inc.amount, 0);
+        }
+      },
+      totalExpenses: {
+        type: Number,
+        default: function() {
+          return this.expenses.reduce((acc, exp) => acc + exp.amount, 0);
+        }
+      },
+
+      // createdBy: {
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   // ref: 'User',
+      //   // required: true
+      // }
 });
+
+// budgetSchema.virtual('creatorEmail', {
+//     ref: 'User',
+//     localField: 'createdBy',
+//     foreignField: '_id',
+//     justOne: true,
+//     select: 'email'
+// });
 
 
 const Budget = mongoose.model('Budget', budgetSchema);
---
+++ frontend/src/User/components/logout.js
@@ -0,0 +1,11 @@
+
+import Cookies from "js-cookie"
+
+const Logout = () => {
+    Cookies.remove("accessToken")
+    Cookies.remove("role")
+    Cookies.remove("id")
+    window.location.href = "/"
+}
+
+export default Logout
