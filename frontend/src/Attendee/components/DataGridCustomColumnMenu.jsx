49489698a0b1bf7f6250774cab213594dd6e0f2d
CoorayNTL <tl685998@gmail.com>
2023-05-16 17:00:51 +0530

    updated datafinlistic

--- frontend/src/Attendee/components/DataGridCustomColumnMenu.jsx
+++ frontend/src/Attendee/components/DataGridCustomColumnMenu.jsx
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 import React from 'react'
-=======
-
-import {
-  GridColumnMenuContainer,
-  GridFilterMenuItem,
-  HideGridColMenuItem,
-} from "@mui/x-data-grid";
->>>>>>> 33008fd06f81329b056c9b90f8421d6c121d71d8
 
 const DataGridCustomColumnMenu = () => {
   return (
@@ -15,8 +6,4 @@ const DataGridCustomColumnMenu = () => {
   )
 }
 
-<<<<<<< HEAD
 export default DataGridCustomColumnMenu
-=======
-export default CustomColumnMenu;
->>>>>>> 33008fd06f81329b056c9b90f8421d6c121d71d8
