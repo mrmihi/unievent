981571fe8c8820fc8b96ea8b9715e39e90fb9f01
Shabina <shabinafarveen@gmail.com>
2023-05-16 18:07:36 +0530

    Request Appointment completed with frontend validations

--- frontend/src/Org/OrgLogin.jsx
+++ frontend/src/Org/OrgLogin.jsx
@@ -66,9 +66,9 @@ export default function OLoginPage() {
         console.log(response.data);
         console.log(_id);
 
-        Cookies.set('id', _id, { expires: 1 });
-        Cookies.set('accessToken', accessToken, { expires: 1 });
-        Cookies.set('name', name, { expires: 1 });
+        Cookies.set('org_id', _id, { expires: 1 });
+        Cookies.set('org_accessToken', accessToken, { expires: 1 });
+        Cookies.set('org_name', name, { expires: 1 });
 
         navigate('/org/dashboard/');
       } catch (error) {
