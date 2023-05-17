db3e7fe8b068f5a542e553b37f36c4de7f800f13
Radser2001 <supuni.randeniya@gmail.com>
2023-05-17 20:06:44 +0530

    Update tables

--- frontend/src/Partners/organizer/speakers/AllSpeakers.jsx
+++ frontend/src/Partners/organizer/speakers/AllSpeakers.jsx
@@ -201,12 +201,6 @@ const AllSpeakers = () => {
   const columns = useMemo(
     () => [
       {
-        accessorKey: null,
-        header: 'ID',
-        Cell: ({ row }) => row.index + 1,
-        size: 5,
-      },
-      {
         accessorKey: '_id',
         header: 'ID',
         enableColumnOrdering: false,
