f9c83294880eda2d563520cd46794f353974e343
Pasindu Dinal Sumanasekara <51123579+mrmihi@users.noreply.github.com>
2023-05-17 09:01:12 +0530

    Feat/Added model  validation

--- Event/models/event.model.js
+++ Event/models/event.model.js
@@ -26,10 +26,12 @@ const eventSchema = new Schema(
     name: {
       type: String,
       required: true,
+      minlength: [5, 'Name should have a minimum of 10 characters'],
     },
     description: {
       type: String,
       required: true,
+      minlength: [10, 'Description should have a minimum of 10 characters'],
     },
     headerImage: {
       type: String,
@@ -41,10 +43,24 @@ const eventSchema = new Schema(
     startTime: {
       type: Date,
       required: true,
+      validate: {
+        validator: function (value) {
+          // Ensure the startTime is not in the past
+          return value >= new Date();
+        },
+        message: 'Start Date & Time should be in the future',
+      },
     },
     endTime: {
       type: Date,
       required: true,
+      validate: {
+        validator: function (value) {
+          // Ensure the endTime is not in the past
+          return value >= new Date();
+        },
+        message: 'End Date & Time should be in the future',
+      },
     },
     status: {
       type: String,
@@ -65,6 +81,8 @@ const eventSchema = new Schema(
     },
     capacity: {
       type: Number,
+      min: [10, 'Capacity should be at least 10'],
+      max: [1000, 'Capacity should be at most 1000'],
     },
     tags: {
       type: [String],
